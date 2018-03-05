ef6cd9a314dd808fa40b28dec33d5d57f9843bec
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 10:03:00 +0800

    Add files via upload

--- linux/linklist/Insert/Insert_head.c
+++ linux/linklist/Insert/Insert_head.c
@@ -1,1 +1,12 @@
+#include<stdlib.h>
+#include"../Include/list.h"
+bool Insert_head(List plist,int val)
+{
+	Node* p=(Node*)malloc(sizeof(Node));
+	p->data=val;
 
+	p->next=plist->next;
+	plist->next=p;
+	
+	return true;
+}
