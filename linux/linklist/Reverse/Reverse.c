3f76c6491ba123d39e075927c762f83056f89a0a
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 10:33:10 +0800

    Add files via upload

--- linux/linklist/Reverse/Reverse.c
+++ linux/linklist/Reverse/Reverse.c
@@ -1,1 +1,20 @@
-
+#include<stdlib.h>
+#include"../Include/list.h"
+void Reverse(List plist)
+{
+	if(plist==NULL || plist->next==NULL || plist->next->next==NULL)
+	{
+		return;
+	}
+	Node *p=plist->next;
+	plist->next=NULL;
+	Node* q;
+	while(p!=NULL)
+	{
+		q=p->next;
+		
+		p->next=plist->next;
+		plist->next=p;
+		p=q;
+	}
+}
