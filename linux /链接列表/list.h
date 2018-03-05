47a31213de119275585d9eeb483374087cc19a37
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 09:52:30 +0800

    Add files via upload

--- linux /链接列表/list.h
+++ linux /链接列表/list.h
@@ -1,1 +1,32 @@
+typedef struct Node
+{
+	int data; 
+	struct Node* next;
+}Node,*List;
+
+#define bool int
+#define true 1
+#define false 0
+
+void Initlist(List plist);
+
+bool Insert_head(List plist,int val);
+
+bool Insert_tail(List plist,int val);
+
+bool Insert_pos(List plist,int pos,int val);
+	
+bool Delete_head(List plist);
+
+bool Delete_tail(List plist);
+
+bool Delete_pos(List plist,int pos);
+
+Node* Find_val(List plist,int val);
+
+void Reverse(List plist);
+
+
+
+
 
