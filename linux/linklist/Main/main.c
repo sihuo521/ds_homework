c0a4b362d667f804d87b55014cf975e58b3e4cb5
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 10:32:32 +0800

    Add files via upload

--- linux/linklist/Main/main.c
+++ linux/linklist/Main/main.c
@@ -1,1 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"../Include/list.h"
+void Initlist(List plist)
+{
+	if(plist==NULL)
+	{
+		return;
+	}
+	plist->next=NULL;
+}
 
+void Show(List plist)
+{
+	Node* p;
+	for(p=plist->next;p!=NULL;p=p->next)
+	{
+		printf("%d ",p->data);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	Node head;
+	Initlist(&head);
+	int i;
+	for(i=0;i<10;i++)
+	{
+		Insert_tail(&head,i);
+	}
+	Show(&head);
+	Insert_head(&head,10);
+
+	Show(&head);
+	Insert_pos(&head,2,11);
+	Show(&head);
+	Delete_head(&head);
+	Show(&head);
+	Delete_tail(&head);
+	Show(&head);
+	Delete_pos(&head,2);
+	Show(&head);
+	Node* p=Find_val(&head,4);
+	printf("%d\n",p->data);
+	Reverse(&head);
+	Show(&head);
+}
