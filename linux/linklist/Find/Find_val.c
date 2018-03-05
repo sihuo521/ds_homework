3fc9151a1f985345d63ab2148b70fee6c9aa9587
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 10:08:19 +0800

    Add files via upload

--- linux/linklist/Find/Find_val.c
+++ linux/linklist/Find/Find_val.c
@@ -1,1 +1,14 @@
-
+#include<stdlib.h>
+#include"../Include/list.h"
+Node* Find_val(List plist,int val)
+{
+	Node *p;
+	for(p=plist->next;p!=NULL;p=p->next)
+	{
+		if(p->data==val)
+		{
+			return p;
+		}
+	}
+	return NULL;
+}
