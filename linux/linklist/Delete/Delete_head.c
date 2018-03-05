ade68296b9c6d0b04b0bdc54f0ac1b8c2b2cc981
sihuo521 <36940636+sihuo521@users.noreply.github.com>
2018-03-05 10:06:29 +0800

    Add files via upload

--- linux/linklist/Delete/Delete_head.c
+++ linux/linklist/Delete/Delete_head.c
@@ -1,1 +1,13 @@
-
+#include<stdlib.h>
+#include"../Include/list.h"
+bool Delete_head(List plist)
+{
+	if(plist->next==NULL)
+	{
+		return false;
+	}
+	Node* q=plist->next;
+	plist->next=q->next;
+	free(q);
+	return true;
+}
